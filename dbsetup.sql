0a8927bb66169062200a29399d002512a437060e
martin <martin@hpDesktop.(none)>
2015-10-15 03:07:01 +0300

    db setup, composer updated

---
+++ dbsetup.sql
@@ -0,0 +1,57 @@
+
+BEGIN;
+
+CREATE TABLE "user"
+(
+  user_sid serial NOT NULL,
+  email character varying(300) NOT NULL,
+  password character varying(50) NOT NULL,
+  CONSTRAINT pkey_user_sid PRIMARY KEY (user_sid ),
+  CONSTRAINT uniq_user_email UNIQUE (email )
+)
+WITH (
+  OIDS=FALSE
+);
+
+INSERT INTO "user" (email, password) VALUES ('your@email.com', MD5('password'));
+
+CREATE TABLE project
+(
+  project_sid serial NOT NULL,
+  name character varying(100) NOT NULL,
+  CONSTRAINT pkey_project_sid PRIMARY KEY (project_sid )
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE todo_status
+(
+  todo_status_sid serial NOT NULL,
+  name character varying(20),
+  CONSTRAINT pkey_todo_status_sid PRIMARY KEY (todo_status_sid )
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE todo
+(
+  todo_sid serial NOT NULL,
+  project_sid integer,
+  priority integer NOT NULL DEFAULT 0,
+  todo_status_sid integer NOT NULL,
+  text text,
+  date_created timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT pkey_todo_sid PRIMARY KEY (todo_sid ),
+  CONSTRAINT fkey_todo_todo_status_sid_todo_status_todo_status_sid FOREIGN KEY (todo_status_sid)
+      REFERENCES todo_status (todo_status_sid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+
+COMMIT;
